eac72a8e6159c8603440784d8dd77737edc8d7f7
Jacob Sensing <jsensing176@gmail.com>
2024-03-28 17:20:11 -0500

    added nobles.txt and nobles class

--- pieces.py
+++ pieces.py
@@ -1,3 +1,5 @@
+import random
+
 class Gem:
     def __init__(self, name, points):
         self.name = name
@@ -6,21 +8,59 @@ class Gem:
     def __str__(self):
         return f"{self.points} {self.name}"
 
+
 class Card:
-    def __init__(self, level, color, vp, black, blue, green, red, white):
+    def __init__(self, level, color, vp, onyx, sapphire, emerald, ruby, diamond):
         self.level = level
         self.color = color
         self.vp = vp
-        self.black = black
-        self.blue = blue
-        self.green = green
-        self.red = red
-        self.white = white
+        self.onyx = onyx
+        self.sapphire = sapphire
+        self.emerald = emerald
+        self.ruby = ruby
+        self.diamond = diamond
 
     def __str__(self):
-        return f"Level: {self.level} Color: {self.color} VP: {self.vp}, Black: {self.black}, Blue: {self.blue}, Green: {self.green}, Red: {self.red}, White: {self.white}"
+        return f"Level: {self.level} Color: {self.color} VP: {self.vp}, Black: {self.onyx}, Blue: {self.sapphire}, Green: {self.emerald}, Red: {self.ruby}, White: {self.diamond}"
 
-""" def process_file(input_file):
+class Noble:
+    def __init__(self, ID, diamond, sapphire, emerald, ruby, onyx, pointValue):
+        self.ID = ID
+        self.diamond = diamond
+        self.sapphire = sapphire
+        self.emerald = emerald
+        self.ruby = ruby
+        self.onyx = onyx
+        self.pointValue = pointValue
+
+    def __str__(self):
+        return f"ID: {self.ID}, Diamond: {self.diamond}, Sapphire: {self.sapphire}, Emerald: {self.emerald}, Ruby: {self.ruby}, Onyx: {self.onyx}, Point Value: {self.pointValue}"
+
+class Nobles:
+    def __init__(self, filename):
+        self.nobles_list = []
+        self.load_nobles(filename)
+
+    def load_nobles(self, filename):
+        with open(filename, 'r') as f:
+            for line in f:
+                ID, diamond, sapphire, emerald, ruby, onyx, pointValue = line.strip().split(',')
+                noble = Noble(ID, int(diamond), int(sapphire), int(emerald), int(ruby), int(onyx), int(pointValue))
+                self.nobles_list.append(noble)
+
+    def get_random_nobles(self, num_nobles=3):
+        return random.sample(self.nobles_list, num_nobles)
+
+# Example usage
+nobles_obj = Nobles(r'C:\Users\wrait\OneDrive\Desktop\PySplendor\nobles.txt')
+random_nobles = nobles_obj.get_random_nobles()
+
+print("Random Nobles:")
+for noble in random_nobles:
+    print(noble)
+
+
+def process_file(input_file):
     level1_deck = []
     level2_deck = []
     level3_deck = []
@@ -31,13 +71,13 @@ class Card:
             level = int(data[0])
             color = data[1]
             vp = int(data[2])
-            black = int(data[3])
-            blue = int(data[4])
-            green = int(data[5])
-            red = int(data[6])
-            white = int(data[7])
+            onyx = int(data[3])
+            sapphire = int(data[4])
+            emerald = int(data[5])
+            ruby = int(data[6])
+            diamond = int(data[7])
 
-            card = Card(level, color, vp, black, blue, green, red, white)
+            card = Card(level, color, vp, onyx, sapphire, emerald, ruby, diamond)
             if level == 1:
                 level1_deck.append(card)
             elif level == 2:
@@ -61,11 +101,11 @@ for card in level2_deck:
 print("\nLevel 3 Deck:")
 for card in level3_deck:
     print(card)
-"""
+
 
 
 # Create Gem objects for each token type
-"""ruby = Gem("Ruby", 1)
+ruby = Gem("Ruby", 1)
 sapphire = Gem("Sapphire", 1)
 diamond = Gem("Diamond", 1)
 onyx = Gem("Onyx", 1)
@@ -77,4 +117,4 @@ gems_array = [ruby, sapphire, diamond, onyx, emerald, wild]
 
 # Print the array of Gem objects
 for gem in gems_array:
-    print(gem) """
+    print(gem)
