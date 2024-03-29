0ebfb0790464970f8f2a370c6f88f68d66145fd7
Jsensing <88456999+Jsensing@users.noreply.github.com>
2024-03-28 20:40:49 -0500

    Mechanics - Pull request (#1)
    * added Gems class
    
    * added Card class
    
    * added cards.txt and function to create decks
    
    * added nobles.txt and nobles class
    
    * added Player class and some functions associated
    
    * changed file name
    
    * starting setup process
    
    * added more mechanics
    
    * added more mechanics

---
+++ pieces.py
@@ -0,0 +1,43 @@
+import random
+
+class Card:
+    def __init__(self, level, color, vp, onyx, sapphire, emerald, ruby, diamond):
+        self.level = level
+        self.color = color
+        self.vp = vp
+        self.onyx = onyx
+        self.sapphire = sapphire
+        self.emerald = emerald
+        self.ruby = ruby
+        self.diamond = diamond
+
+    def __str__(self):
+        return f"Level: {self.level} Color: {self.color} VP: {self.vp}, Black: {self.onyx}, Blue: {self.sapphire}, Green: {self.emerald}, Red: {self.ruby}, White: {self.diamond}"
+
+class Noble:
+    def __init__(self, ID, diamond, sapphire, emerald, ruby, onyx, pointValue):
+        self.ID = ID
+        self.diamond = diamond
+        self.sapphire = sapphire
+        self.emerald = emerald
+        self.ruby = ruby
+        self.onyx = onyx
+        self.pointValue = pointValue
+
+    def __str__(self):
+        return f"ID: {self.ID}, Diamond: {self.diamond}, Sapphire: {self.sapphire}, Emerald: {self.emerald}, Ruby: {self.ruby}, Onyx: {self.onyx}, Point Value: {self.pointValue}"
+
+class Nobles:
+    def __init__(self, filename):
+        self.nobles_list = []
+        self.load_nobles(filename)
+
+    def load_nobles(self, filename):
+        with open(filename, 'r') as f:
+            for line in f:
+                ID, diamond, sapphire, emerald, ruby, onyx, pointValue = line.strip().split(',')
+                noble = Noble(ID, int(diamond), int(sapphire), int(emerald), int(ruby), int(onyx), int(pointValue))
+                self.nobles_list.append(noble)
+
+    def get_random_nobles(self, num_nobles=3):
+        return random.sample(self.nobles_list, num_nobles)
