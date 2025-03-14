ef99d80fd5664780727277a338aa439b151910ed
Jacob Sensing <jsensing176@gmail.com>
2025-03-14 04:08:24 -0500

    added logic to read CSV file to load cards.

---
+++ src/load_cards.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from src.card import DevelopmentCard
+
+def load_cards_from_csv(file_path):
+    df = pd.read_csv(file_path, names=["level", "color", "costWhite", "costBlue", "costGreen", "costRed", "costBlack", "bonus"])
+
+    cards = []
+    for _, row in df.iterrows():
+        cost = {
+            "White": int(row["costWhite"]),
+            "Blue": int(row["costBlue"]),
+            "Green": int(row["costGreen"]),
+            "Red": int(row["costRed"]),
+            "Black": int(row["costBlack"]),
+        }
+        card = DevelopmentCard(level=row["level"], color=row["color"], cost=cost, bonus=row["bonus"])
+        cards.append(card)
+
+    return cards
